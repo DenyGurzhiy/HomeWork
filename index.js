299f4a9926e0a9918e3a3e9ac93defaa00e1fcb0
denygurzhiy <denys.gurzhiy@gmail.com>
2022-09-07 11:44:13 +0200

    Add test case #3

--- index.js
+++ index.js
@@ -66,17 +66,3 @@ export function TheLargestNumber(a1, b1, c1) {
 }
 
 
-//   if (isATheLargest(a, b, c)) {
-//     return "The 1st number is largest and equal: " + a;
-//   } else if (isBTheLargest(a, b, c)) {
-//     return "The 2nd number is largest and equal: " + b;
-//   } else if (isCTheLargest(a, b, c)) {
-//     return "The 3rd number is largest and equal: " + c;
-//   } else if (isTwoEqualNumb(a, b, c) != "") {
-//     return (`The ${isTwoEqualNumb(a, b, c)[0]} numbers are equal and amount is: ${isTwoEqualNumb(a, b, c)[1]}`);
-//   }
-//   else {
-//     return "All three numbers are equal. And equal: " + a;
-//   }
-// }
-console.log(TheLargestNumber(7.7, '7.7', '7.6'));
--- test.js
+++ test.js
@@ -7,3 +7,7 @@ console.log(areEqual1);
 const result2 = TheLargestNumber(7.7, '7.7', '7.6');
 const areEqual2 = result2 === "The 1st and 2nd numbers are equal and amount is: 7.7";
 console.log(areEqual2);
+
+const result3 = TheLargestNumber(7.7, '7.7', '7.7');
+const areEqual3 = result3 === "All three numbers are equal. And equal: 7.7";
+console.log(areEqual3);
