445cb18abbaeeb8b9a974f228b9473d75ee70fae
denygurzhiy <denys.gurzhiy@gmail.com>
2022-09-21 20:34:40 +0200

    Update

--- Les11/javascript.js
+++ Les11/javascript.js
@@ -9,25 +9,25 @@
 
 
 // // Declaring arrays
-// const fruits = ['Apples', 'Tangerines', 'Grapes', 'Pears'];
-// const dairy = ['Cheese', 'Milk', 'Yoghurt'];
-// const vegetables = ['Tomatoes', 'Cucumbers', 'Carrots'];
-// const drinks = ['Mineral water', 'Juice', 'Vine', 'Coca-Cola'];
+const fruits = ['Apples', 'Tangerines', 'Grapes', 'Pears'];
+const dairy = ['Cheese', 'Milk', 'Yoghurt'];
+const vegetables = ['Tomatoes', 'Cucumbers', 'Carrots'];
+const drinks = ['Mineral water', 'Juice', 'Vine', 'Coca-Cola'];
 
-// const goods = fruits.concat(dairy, vegetables, drinks);
+const goods = fruits.concat(dairy, vegetables, drinks);
 
 
 // // Declaring function which accepts text and outputs <li> html element
-// function createListItem(text) {
-//     // Creating <li> element
-//     const listItem = document.createElement('li');
-//     // Creating text node
-//     const textNode = document.createTextNode(text);
-//     // Putting text node inside of <li> HTML element
-//     listItem.appendChild(textNode);
-//     // Returning listItem
-//     return listItem;
-// }
+function createListItem(text) {
+    // Creating <li> element
+    const listItem = document.createElement('li');
+    // Creating text node
+    const textNode = document.createTextNode(text);
+    // Putting text node inside of <li> HTML element
+    listItem.appendChild(textNode);
+    // Returning listItem
+    return listItem;
+}
 
 // // Getting <ul> element from HTML to which we will attach list elements later
 // const list = document.getElementById('shopping-list');
